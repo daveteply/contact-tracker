e17c316a14631caa9de65bf433b26930920d9fde
Dave Teply <davehamdan@gmail.com>
2025-12-01 15:58:19 -0600

    arch: containerize .net service

--- services/tracker-api/Program.cs
+++ services/tracker-api/Program.cs
@@ -16,8 +16,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
-    "one", "two"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
 };
 
 app.MapGet("/weatherforecast", () =>
