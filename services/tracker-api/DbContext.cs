3c03a87d57635d20b0cc32284cb8b32f75431bf4
Dave Teply <davehamdan@gmail.com>
2025-12-16 14:28:51 -0600

    WIP: laptop changes

--- services/tracker-api/DbContext.cs
+++ services/tracker-api/DbContext.cs
@@ -1,18 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 
-public class ContractTrackerContext : DbContext
+public class ContractTrackerDbContext : DbContext
 {
-    public ContractTrackerContext(DbContextOptions<ContractTrackerContext> options)
+    public ContractTrackerDbContext(DbContextOptions<ContractTrackerDbContext> options)
         : base(options)
     {
-    }
-
-    protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseNpgsql("Host=postgres;Port=5432;Database=mydb;Username=myuser;Password=mypassword");
-        }
     }
 
     public DbSet<Contact> Contracts { get; set; }
@@ -21,7 +13,10 @@ public class ContractTrackerContext : DbContext
 public class Contact
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public required string Email { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Title { get; set; }
+    public string Email { get; set; } = null!;
     public string? PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
--- services/tracker-api/Program.cs
+++ services/tracker-api/Program.cs
@@ -1,17 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContractTrackerDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("ContractTrackerContext"))
+    );
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
