1c2787739a09f34ba43948a57d57686652ed4173
Dave Teply <davehamdan@gmail.com>
2026-01-17 09:58:09 +0000

    feat: updating event list

--- packages/ui-shared/src/lib/event/event-list.tsx
+++ packages/ui-shared/src/lib/event/event-list.tsx
@@ -1,7 +1,7 @@
-import { EventReadDto } from '@contact-tracker/api-models';
+import { EventReadDtoWithRelations } from '@contact-tracker/api-models';
 
 export interface EventListProps {
-  eventList: any;
+  eventList: EventReadDtoWithRelations[];
 }
 
 export function EventList(props: EventListProps) {
@@ -9,15 +9,48 @@ export function EventList(props: EventListProps) {
     <div className="mt-5 flex flex-wrap">
       {props.eventList && props.eventList.length ? (
         <>
-          {props.eventList.map((event: EventReadDto) => (
+          {props.eventList.map((event: EventReadDtoWithRelations) => (
             <div
-              className='className="card bg-neutral text-neutral-content w-64 shadow-sm mb-3 mr-3"'
+              className="card bg-neutral text-neutral-content w-64 shadow-sm mb-3 mr-3"
               key={event.id}
             >
               <div className="card-body">
                 <ul>
-                  <li>{event.companyId}</li>
-                  <li>{event.contactId}</li>
+                  <li>
+                    <div className="card card-xs">
+                      <div className="card-body">
+                        <h2 className="card-title">{event.company?.name}</h2>
+                      </div>
+                      <ul>
+                        <li>
+                          <a href={event.company?.website}>{event.company?.website}</a>
+                        </li>
+                        <li>{event.company?.industry}</li>
+                        <li>{event.company?.sizeRange}</li>
+                        <li>{event.company?.notes}</li>
+                      </ul>
+                    </div>
+                  </li>
+
+                  <li>
+                    <div className="card card-xs">
+                      <div className="card-body">
+                        <h2 className="card-title">
+                          {event.contact?.firstName} {event.contact?.lastName}
+                        </h2>
+                        <h3>{event.contact?.title}</h3>
+                      </div>
+                      <ul>
+                        <li>
+                          <a href={`mailto:${event.contact?.email}`}>{event.contact?.email}</a>
+                        </li>
+                        <li>{event.contact?.linkedInUrl}</li>
+                        <li>{event.contact?.phoneNumber}</li>
+                        <li>{event.contact?.notes}</li>
+                      </ul>
+                    </div>
+                  </li>
+
                   <li>{event.roleId}</li>
                   <li>{event.eventTypeId}</li>
                   {/* <li>{event.occurredAt}</li> */}
