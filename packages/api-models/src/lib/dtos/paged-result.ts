e3278775cc64f60df134de054426eed91232f1fd
Dave Teply <davehamdan@gmail.com>
2026-01-28 13:09:57 +0000

    moving UI dtos to folder

---
+++ packages/api-models/src/lib/dtos/paged-result.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { PaginationMetadata } from "./pagination-metadata";
+
+export interface PagedResult<T> {
+    data: T[];
+    pagination: PaginationMetadata;
+}
