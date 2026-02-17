32e48b3d88721390f690985d029681b19fb7d5c1
Dave Teply <davehamdan@gmail.com>
2026-02-17 19:35:52 +0000

    regenerated dto

--- packages/api-models/src/lib/contact-document-dto.ts
+++ packages/api-models/src/lib/contact-document-dto.ts
@@ -3,16 +3,16 @@
  * Any changes made to this file can be lost when this file is regenerated.
  */
 
-import { BaseDocumentDto } from './base-document-dto';
+import { BaseDocumentDto } from "./base-document-dto";
 
 export interface ContactDocumentDto extends BaseDocumentDto {
-  companyId?: string;
-  firstName: string;
-  lastName: string;
-  title?: string;
-  email?: string;
-  phoneNumber?: string;
-  linkedInUrl?: string;
-  isPrimaryRecruiter: boolean;
-  notes?: string;
+    companyId?: string;
+    firstName: string;
+    lastName: string;
+    title?: string;
+    email?: string;
+    phoneNumber?: string;
+    linkedInUrl?: string;
+    isPrimaryRecruiter?: boolean;
+    notes?: string;
 }
